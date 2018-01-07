16c11018703de1bab9bb0605bc304926b5ea260b
Daniele <peppe877@gmail.com>
2018-01-07 19:05:20 +0100

    coso

--- manage.py
+++ manage.py
@@ -1,19 +1,19 @@
 import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from flask.ext.script import Manager, Server
-from flask.ext.migrate import MigrateCommand
-from flask_blog import app
+from flask_script import Manager, Server
+#from flask_migrate import MigrateCommand
+from pobuilds import app
 
 manager = Manager(app)
 
-manager.add_command('db', MigrateCommand)
+#manager.add_command('db', MigrateCommand)
 
 manager.add_command('runserver', Server(
     use_debugger = True,
     use_reloader = True,
-    host = os.getenv('IP', '0.0.0.0'),
-    port = int(os.getenv('PORT', '5000'))
+    #host = os.getenv('IP', '0.0.0.0'),
+    #port = int(os.getenv('PORT', '5000'))
     )
 )
 
