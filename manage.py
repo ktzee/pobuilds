46ffb97615fa25dfa0560f0d3160eca14fe151d7
Daniele Consoli <dconsoli@redhat.com>
2018-03-10 13:08:30 +0100

    working wsgi

--- manage.py
+++ manage.py
@@ -12,11 +12,11 @@ manager = Manager(app)
 manager.add_command('runserver', Server(
     use_debugger = True,
     use_reloader = True,
-    #host = os.getenv('IP', '0.0.0.0'),
-    #port = int(os.getenv('PORT', '5000'))
+    host = os.getenv('IP', '0.0.0.0'),
+    port = int(os.getenv('PORT', '8000'))
     )
 )
 
 if __name__ == "__main__":
     manager.run()
-    
+
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
-from pobuilds import app
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from pobuilds import app as application
+
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
